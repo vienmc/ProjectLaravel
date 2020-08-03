59bdb4d7a11b2e9d6a67984aa931004bae2dbd6a
trinhlhth1908021@fpt.edu.vn <Quangbinh123@>
2020-08-03 20:44:52 +0700

    +sửa layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -253,52 +253,9 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-3">
-                <div class="left-sidebar">
-                    <h2>Danh mục sản phẩm</h2>
-                    <div class="panel-group category-products" id="accordian"><!--category-productsr-->
-                        @foreach($category as $key => $cate)
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title"><a
-                                            href="{{URL::to('/danh-muc-san-pham/'.$cate->id)}}">{{$cate->name}}</a>
-                                    </h4>
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
-                    <!--/category-products-->
 
-                    <div class="brands_products"><!--brands_products-->
-                        <h2>Thương hiệu sản phẩm</h2>
-                        @foreach($brand as $key => $brand_product)
-                            <div class="brands-name">
-                                <ul class="nav nav-pills nav-stacked">
-                                    <li><a href="{{URL::to('/thuong-hieu-san-pham/'.$brand_product->id)}}">
-                                            <span class="pull-right">
-                                            @foreach($brand_product->product_brand as $count_brand)
-                                                    {{$count_brand->brand_id}}
-                                                @endforeach
-                                            </span>{{$brand_product->brand_name}}
-                                        </a></li>
-                                </ul>
-                            </div>
-                        @endforeach
-                    </div><!--/brands_products-->
+                @yield('left-sidebar')
 
-                    <div class="price-range"><!--price-range-->
-                        <h2>Price Range</h2>
-                        <div class="well text-center">
-                            <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="600"
-                                   data-slider-step="5" data-slider-value="[250,450]" id="sl2"><br/>
-                            <b class="pull-left">$ 0</b> <b class="pull-right">$ 600</b>
-                        </div>
-                    </div><!--/price-range-->
-
-                    <div class="shipping text-center"><!--shipping-->
-                        <img src="{{asset('frontend/images/shipping.jpg')}}" alt=""/>
-                    </div><!--/shipping-->
-
-                </div>
             </div>
 
             <div class="col-sm-9">
